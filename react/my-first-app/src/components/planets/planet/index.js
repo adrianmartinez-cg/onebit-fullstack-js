8b3d115425d9e5df72ea5592b781c259b4d3f8ea
Pedro Adrian <pedro.martinez@ccc.ufcg.edu.br>
2023-02-21 00:02:30 -0300

    react: lifecycle and states

--- react/my-first-app/src/components/planets/planet/index.js
+++ react/my-first-app/src/components/planets/planet/index.js
@@ -1,17 +1,39 @@
 import React, {Fragment} from 'react';
 import DescriptionWithLink from '../../shared/DescriptionWithLink';
 
-const Planet = (props) => {
-    return (
-        <Fragment>
-            <h4>{props.name}</h4>
-            <img src={props.imgUrl} width= "250px" alt={"foto de "+ props.name}></img>
-            <DescriptionWithLink 
-                text={props.text}
-                link={props.link}
-            />
-        </Fragment>
-    )
-};
+class Planet extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            satellites: props.satellites
+        }
+        this.name = props.name;
+        this.imgUrl = props.imgUrl;
+        this.description = props.description;
+        this.link = props.link;
+    }
+
+    render(){
+        
+            return (
+                <Fragment>
+                    <h4>{this.name}</h4>
+                    <img src={this.imgUrl} width= "250px" alt={"foto de "+ this.name}></img>
+                    <DescriptionWithLink 
+                        text={this.description}
+                        link={this.link}
+                    />
+                    <p> Satélites: </p>
+                    <ul>
+                        {
+                            this.state.satellites.map(s => <li>{s}</li>)
+                        }
+                    </ul>
+                </Fragment>
+            )
+        
+    }
+}
 
 export default Planet;
